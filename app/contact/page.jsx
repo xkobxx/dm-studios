983ecabf53ceaf4f0cee80e602fe858ac8bb4582
David Mensah <13025001+xkobxx@users.noreply.github.com>
2026-02-28 20:35:24 +0000

    fixed the form api

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -5,6 +5,8 @@ import Social from "@/components/Social";
 import Link from "next/link";
 import { FaEnvelope, FaGithub, FaLinkedinIn } from "react-icons/fa";
 
+export const dynamic = 'force-dynamic';
+
 const Contact = () => {
   return (
     <section className="py-12 md:py-20">
